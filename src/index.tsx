3a1325cbf8a84bb349a14e41968a255a9bc2b2c4
KamilKmita <kamilkmita15@gmail.com>
2023-03-16 23:08:22 +0100

    init app

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+reportWebVitals();
